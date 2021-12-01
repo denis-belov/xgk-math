5740138c423f0635ab9615d5875e457b5f0512ec
dsbelov63 <dsbelov63@gmail.com>
2021-12-01 11:17:08 +0400

    up

--- src/_intrin.h
+++ src/_intrin.h
@@ -8,9 +8,9 @@
 
 	#define __m128 __f32x4
 	#define _mm_load_ps wasm_v128_load
-	#define _mm_load_ps1 wasm_v32x4_load_splat
+	#define _mm_load_ps1 wasm_v128_load32_splat
 	#define _MM_SHUFFLE(a, b, c, d) (a * 4) + 0, (a * 4) + 1, (a * 4) + 2, (a * 4) + 3, (b * 4) + 0, (b * 4) + 1, (b * 4) + 2, (b * 4) + 3, (c * 4) + 0, (c * 4) + 1, (c * 4) + 2, (c * 4) + 3, (d * 4) + 0, (d * 4) + 1, (d * 4) + 2, (d * 4) + 3
-	#define _mm_shuffle_ps __builtin_wasm_shuffle_v8x16
+	#define _mm_shuffle_ps __builtin_wasm_shuffle_i8x16
 	// make wasm_f32x4_make(1.0f, -1.0f, 1.0f, -1.0f) constant
 	#define _mm_addsub_ps(a, b) wasm_f32x4_add(a, wasm_f32x4_mul(b, wasm_f32x4_make(1.0f, -1.0f, 1.0f, -1.0f)))
 	#define _mm_store_ps wasm_v128_store
